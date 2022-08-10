9c2ac6f2b87da390971b432c7b5f4cffe5ca9e55
superlbr <superlbr@126.com>
2022-08-10 11:51:28 +0800

    chore file: clean

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -1,18 +1,8 @@
 import { Controller } from 'egg';
-// const DbLevel = require('../core/level')
+
 export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
     ctx.body = await ctx.service.test.sayHi('egg');
   }
-
-  // async test2() {
-  //   const { ctx } = this
-
-  //   await DbLevel.put('foo', {a: 123})
-  //   const value = await DbLevel.get('foo')
-
-  //   console.log(value)
-  //   ctx.body = value
-  // }
 }
